454599788bfed750b510744e71545044f360ff97
ChrisLyu7 <911273708@qq.com>
2022-07-13 01:43:46 +0800

    hello world

--- main.py
+++ main.py
@@ -14,5 +14,6 @@ if __name__ == '__main__':
     print('good')
     print('good good')
     print('good good hello')
+    print("hello world")
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
